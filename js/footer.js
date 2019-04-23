29a5d9069e4d9eacf35094a2985ade36b4a3079f
Dani Fernández Alonso <dani@MacBook-Pro-de-Dani.local>
2019-04-23 21:27:04 +0200

    Footer hecho

--- js/footer.js
+++ js/footer.js
@@ -9,3 +9,41 @@ $(document)
                         }
                     );
             });
+
+$(document)
+                .ready(function () {
+                    
+                    $( ".socialnav a.medium" ).hover(
+                        function() {
+                            $(".footer").addClass("footer-medium");
+                        }, function() {
+                            $(".footer").removeClass("footer-medium");
+                        }
+                    );
+            });
+
+            $(document)
+                .ready(function () {
+                    
+                    $( ".socialnav a.twitter" ).hover(
+                        function() {
+                            $(".footer").addClass("footer-twitter");
+                        }, function() {
+                            $(".footer").removeClass("footer-twitter");
+                        }
+                    );
+            });
+
+            $(document)
+                .ready(function () {
+                    
+                    $( ".socialnav a.instagram" ).hover(
+                        function() {
+                            $(".footer").addClass("footer-instagram");
+                        }, function() {
+                            $(".footer").removeClass("footer-instagram");
+                        }
+                    );
+            });
+
+
