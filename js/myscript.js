653eabf6ef4a0d6d487a8f3c7700cb1e63ef2678
LaMagnetica15 <lamagnetica15@LaMagnetica15.local>
2019-05-16 15:03:15 +0200

    Loading

--- js/myscript.js
+++ js/myscript.js
@@ -86,5 +86,12 @@
       }
     });
   });
-
+// tiempo loading
+$(document).ready(function() {
+	
+  setTimeout(function(){
+    $('body').addClass('loaded');
+    $('h1').css('color','#222222');
+  }, 3000);
   
+});
